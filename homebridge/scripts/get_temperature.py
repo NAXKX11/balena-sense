169ea52c5860154d1137fac1491707f7ea24ff3b
James Wilson <james@drakeapps.com>
2019-05-02 23:09:37 -0500

    Use celsius and let homekit convert it

--- homebridge/scripts/get_temperature.py
+++ homebridge/scripts/get_temperature.py
@@ -13,7 +13,7 @@ influx_client = InfluxDBClient('influxdb', 8086, database='balena-sense')
 
 class getTemperature:
 	def GET(self):
-		result = influx_client.query('SELECT ( last("temperature")  * 9.0 / 5.0) + 32 FROM "balena-sense"')
+		result = influx_client.query('SELECT ( last("temperature") ) FROM "balena-sense"')
 		res = result.items()[0][1]
 		for r in res:
 			return r['last']
